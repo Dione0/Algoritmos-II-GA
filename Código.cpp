8693c0cfbab8423289c196c6ecccde99844881d9
Dione0 <dionebrasilr@gmail.com>
2025-09-20 23:52:14 -0300

    Correções

--- Código.cpp
+++ Código.cpp
@@ -3,47 +3,56 @@
 #include<math.h>
 #include<locale.h>
 
-void cadastrar(int funcionarios[100][6]){
-	int matricula, filhos=0, idade,filhos_menores=0, filhos_maiores=0, anos_empresa=0;  //<--A MATRIZ SE ORGANIZA DA SEGUINTE MANEIRA:  i = matricula || idade funcionario ||tempo de empresa ||  número filhos || filhos menores de idade, || maiores de idade
-	int i, j;                                                     //   j = numero de funcionários                           [0]           [1]                [2]               [3]                       [4]                   [5] 
-		
-	printf("Digite a mátricula do funcionário:");
-	scanf("%d", &matricula);
-	funcionarios[i][j] = matricula;
-	i++;
-	printf("Digite quantos anos tem o funcionário:");
-	scanf("%d", &idade);
-	i++; 
-	printf("Digite quantos anos de empresa tem o funcionário:");
-	scanf("%d", &anos_empresa);
-	j++; 
-	printf("Digite quantos filhos ele(a) tem:");
-	scanf("%d", &filhos);
-	i++;
-	while(filhos > 0){
-        printf("Digite a idade do filho: ");
-        scanf("%d", &idade);
+void cadastrar(int funcionarios[100][6], int *total){
+    int i = 0;
+    int j = *total;
+    int matricula, idade, anos_empresa, filhos, filhos_menores=0, filhos_maiores=0, idadef;
+	
+//A MATRIZ SE ORGANIZA DA SEGUINTE MANEIRA:  i = matricula || idade funcionario ||tempo de empresa ||  n?mero filhos || filhos menores de idade, || maiores de idade
+//   j = numero de funcion?rios                       [0]             [1]                 [2]               [3]                      [4]                   [5] 
 
-        if(idade > 18){
-            funcionarios[i][j] = filhos_menores;
-            i++;
-        }else{
-            funcionarios[i][j] = filhos_maiores;
-			i++;  
-		}
-        filhos--;
-    } 
- 
+    printf("Digite a matrícula do funcionário: ");
+    scanf("%d", &matricula);
+    funcionarios[i++][j] = matricula;
+
+    printf("Digite quantos anos tem o funcionário: ");
+    scanf("%d", &idade);
+    funcionarios[i++][j] = idade;
+
+    printf("Digite quantos anos de empresa tem o funcionário: ");
+    scanf("%d", &anos_empresa);
+    funcionarios[i++][j] = anos_empresa;
+
+    printf("Digite quantos filhos ele(a) tem: ");
+    scanf("%d", &filhos);
+    funcionarios[i++][j] = filhos;
+
+    for(int f = 0; f < filhos; f++){
+        printf("Digite a idade do filho %d: ", f+1);
+        scanf("%d", &idadef);
+        if(idadef > 18)
+            filhos_maiores++;
+        else
+            filhos_menores++;
+    }
+
+    funcionarios[i++][j] = filhos_menores;
+    funcionarios[i++][j] = filhos_maiores;
+
+    (*total)++;  
 }
 
-int listar(int funcionarios[100][6], int *total){
-	int j;
-    printf("\n===== LISTA DE FUNCIONÁRIOS =====\n");
-    printf("Matricula | Idade | Tempo de Empresa | Total Filhos | Filhos Menores | Filhos Maiores\n");
-    printf("%8d | %5d | %15d | %12d | %14d | %13d\n", funcionarios[0][j],  funcionarios[1][j], funcionarios[2][j],  funcionarios[3][j], funcionarios[4][j],  funcionarios[5][j] );
-    j--;
+
+void listar(int funcionarios[100][6], int *total){
+	int j, i;
+	
+	printf("\n===== LISTA DE FUNCIONÁRIOS =====\n");
+	printf("Matrícula | Idade | Tempo de Empresa | Total Filhos | Filhos Menores | Filhos Maiores\n");
+	for (j=0; j<*total; j++){ 
+   
+    printf("%8d | %5d | %15d | %12d | %14d | %13d \n", funcionarios[0][j],  funcionarios[1][j], funcionarios[2][j],  funcionarios[3][j], funcionarios[4][j],  funcionarios[5][j] );
     
-        
+    }    
     
 }
 
@@ -61,20 +70,21 @@ int main(){
    
  do{
    printf("\n===============================================");
-   printf("\nGESTOR DE PRESENTES - AÇO FERRO SERRANO 2025");
+   printf("\nGESTOR DE PRESENTES - AO FERRO SERRANO 2025");
    printf("\n===============================================");
    printf("\n 1 - Cadastrar funcionário");
    printf("\n 2 - Listar funcionários");
    printf("\n 3 - Calcular custo total da empresa");
    printf("\n 4 - Sair");
    printf("\n");
+   printf("\n Escolha uma opção: ");
    scanf("%d", &opcao);
      
    switch(opcao){
-   	case 1:
-   	cadastrar(funcionarios);
-   	printf("%4d", funcionarios);  //  <--- teste de lógica do armazenamento do módulo cadastrar() "Dione"
-   	total++;
+   	case 1:	
+
+   	cadastrar(funcionarios, &total);  //  <--- teste de lógica do armazenamento do módulo cadastrar() "Dione"
+   
    	break;
    	case 2:
    	listar(funcionarios, &total);
@@ -92,5 +102,7 @@ int main(){
 }
 
    
+
+
 
 
